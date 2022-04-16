05691a4f2763c9b63697c4491dba0da09e899f27
324Hz <92825997+win21H2@users.noreply.github.com>
2022-04-16 17:49:27 -0500

    initsetup
    added the "!ls -ltrh /dev/video*" sudo command

---
+++ initsetup.py
@@ -0,0 +1,5 @@
+!pip3 install pyserial
+#We have to install pyserial because it is the library we use for communicating to the Arduino
+
+!ls -ltrh /dev/video*
+#We have to check if we are getting any video inputs from the USB ports
