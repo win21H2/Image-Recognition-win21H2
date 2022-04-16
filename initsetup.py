c0b0ee479609a8d83c4d81246df60e5c923ebf20
324Hz <92825997+win21H2@users.noreply.github.com>
2022-04-16 17:51:52 -0500

    initsetup
    added the imports lines

--- initsetup.py
+++ initsetup.py
@@ -1,5 +1,12 @@
+# Run all of these in this exact order or else the rest of the code will not work
+
 !pip3 install pyserial
-#We have to install pyserial because it is the library we use for communicating to the Arduino
+# We have to install pyserial because it is the library we use for communicating to the Arduino
 
 !ls -ltrh /dev/video*
-#We have to check if we are getting any video inputs from the USB ports
+# We have to check if we are getting any video inputs from the USB ports
+
+from jetcam.usb_camera import USBCamera
+camera = USBCamera(width=224, height=224, capture_device=0)
+camera.running = True
+# We first have to import the USB camera library and then set the values of the camera and run said camera
