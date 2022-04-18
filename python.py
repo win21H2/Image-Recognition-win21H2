53f09af6ab9cd2b53e954476f87b593596d7b89d
324Hz <92825997+win21H2@users.noreply.github.com>
2022-04-18 13:14:24 -0500

    python
    added more comments and cleaned up the code. there are still more changes needed

--- python.py
+++ python.py
@@ -1,144 +1,107 @@
 # Run all of these in this exact order or else the rest of the code will not work
-
 !pip3 install pyserial
-# We have to install pyserial because it is the library we use for communicating to the Arduino
 
 !ls -ltrh /dev/video*
-# We have to check if we are getting any video inputs from the USB ports
 
 from jetcam.usb_camera import USBCamera
 camera = USBCamera(width=224, height=224, capture_device=0)
 camera.running = True
-# We first have to import the USB camera library and then set the values of the camera and run said camera
 
+
+
+
+# create the tasks for image recognition
 import torchvision.transforms as transforms
 from dataset import ImageClassificationDataset
-
 TASK = 'thumbs'
 CATEGORIES = ['thumbs_up', 'thumbs_down']
 DATASETS = ['A', 'B', 'C']
-
 TRANSFORMS = transforms.Compose([
     transforms.ColorJitter(0.2, 0.2, 0.2, 0.2),
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
-
 datasets = {}
 for name in DATASETS:
-    datasets[name] = ImageClassificationDataset('../data/classification/' + TASK + '_' + name, CATEGORIES, TRANSFORMS)
-    
+    datasets[name] = ImageClassificationDataset('../data/classification/' + TASK + '_' + name, CATEGORIES, TRANSFORMS)  
 print("{} task with {} categories defined".format(TASK, CATEGORIES))
 
 
 
 
-
+# make a directory for where we will store the images
 DATA_DIR = '/nvdli-nano/data/classification/'
 !mkdir -p {DATA_DIR}
 
 
 
 
+# create the widgets for the display
 import ipywidgets
 import traitlets
 from IPython.display import display
 from jetcam.utils import bgr8_to_jpeg
-
-# initialize active dataset
 dataset = datasets[DATASETS[0]]
-
-# unobserve all callbacks from camera in case we are running this cell for second time
 camera.unobserve_all()
-
-# create image preview
 camera_widget = ipywidgets.Image()
 traitlets.dlink((camera, 'value'), (camera_widget, 'value'), transform=bgr8_to_jpeg)
-
-# create widgets
 dataset_widget = ipywidgets.Dropdown(options=DATASETS, description='dataset')
 category_widget = ipywidgets.Dropdown(options=dataset.categories, description='category')
 count_widget = ipywidgets.IntText(description='count')
 save_widget = ipywidgets.Button(description='add')
-
-# manually update counts at initialization
 count_widget.value = dataset.get_count(category_widget.value)
-
-# sets the active dataset
 def set_dataset(change):
     global dataset
     dataset = datasets[change['new']]
     count_widget.value = dataset.get_count(category_widget.value)
 dataset_widget.observe(set_dataset, names='value')
-
-# update counts when we select a new category
 def update_counts(change):
     count_widget.value = dataset.get_count(change['new'])
 category_widget.observe(update_counts, names='value')
-
-# save image for category and update counts
 def save(c):
     dataset.save_entry(camera.value, category_widget.value)
     count_widget.value = dataset.get_count(category_widget.value)
 save_widget.on_click(save)
-
 data_collection_widget = ipywidgets.VBox([
     ipywidgets.HBox([camera_widget]), dataset_widget, category_widget, count_widget, save_widget
 ])
-
-# display(data_collection_widget)
 print("data_collection_widget created")
 
 
 
 
-
-
+# Load up the model in this case we are using RESNET 18
 import torch
 import torchvision
-
 device = torch.device('cuda')
-
-# RESNET 18
 model = torchvision.models.resnet18(pretrained=True)
 model.fc = torch.nn.Linear(512, len(dataset.categories))
-    
 model = model.to(device)
-
 model_save_button = ipywidgets.Button(description='save model')
 model_load_button = ipywidgets.Button(description='load model')
 model_path_widget = ipywidgets.Text(description='model path', value='/nvdli-nano/data/classification/my_model.pth')
-
 def load_model(c):
     model.load_state_dict(torch.load(model_path_widget.value))
 model_load_button.on_click(load_model)
-    
 def save_model(c):
     torch.save(model.state_dict(), model_path_widget.value)
 model_save_button.on_click(save_model)
-
 model_widget = ipywidgets.VBox([
     model_path_widget,
     ipywidgets.HBox([model_load_button, model_save_button])
 ])
-
-# display(model_widget)
 print("model configured and model_widget created")
 
 
 
 
-
-
-
-
+# HERE
 import threading
 import time
 from utils import preprocess
 import torch.nn.functional as F
 import serial
-
 prediction_string = ""
 prediction_score = 0
 state_widget = ipywidgets.ToggleButtons(options=['stop', 'live'], description='state', value='stop')
@@ -148,7 +111,6 @@ score_widgets = []
 for category in dataset.categories:
     score_widget = ipywidgets.FloatSlider(min=0.0, max=1.0, description=category, orientation='vertical')
     score_widgets.append(score_widget)
-
 def live(state_widget, model, camera, prediction_widget, score_widget):
     global dataset
     global prediction_string
@@ -164,22 +126,13 @@ def live(state_widget, model, camera, prediction_widget, score_widget):
         prediction_score = list(output)[category_index]
         for i, score in enumerate(list(output)):
             score_widgets[i].value = score    
-            
 def start_live(change):
     if change['new'] == 'live':
         execute_thread = threading.Thread(target=live, args=(state_widget, model, camera, prediction_widget, score_widget))
         execute_thread.start()
-
 def getdata(c):
     with serial.Serial('/dev/ttyACM0', 9600, timeout=10) as ser:
-        ser.write(bytes(1, 'utf-8')); 
-        
-        #if prediction_score >= 0.7 and prediction_string == 'thumbs_up':
-            #ser.write(bytes('YES\n', 'utf-8')) 
-                
-        #if prediction_score >= 0.7 and prediction_string == 'thumbs_down':
-            #ser.write(bytes('NO\n', 'utf-8'))  
-                
+        ser.write(bytes(1, 'utf-8'));       
 output_widget.on_click(getdata)       
 state_widget.observe(start_live, names='value')
 live_execution_widget = ipywidgets.VBox([
@@ -188,41 +141,32 @@ live_execution_widget = ipywidgets.VBox([
     state_widget,
     output_widget
 ])
-
 print("live_execution_widget created")
 
 
 
 
-
-
+# This calculates all of the 
 BATCH_SIZE = 8
-
 optimizer = torch.optim.Adam(model.parameters())
-# optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
-
 epochs_widget = ipywidgets.IntText(description='epochs', value=1)
 eval_button = ipywidgets.Button(description='evaluate')
 train_button = ipywidgets.Button(description='train')
 loss_widget = ipywidgets.FloatText(description='loss')
 accuracy_widget = ipywidgets.FloatText(description='accuracy')
 progress_widget = ipywidgets.FloatProgress(min=0.0, max=1.0, description='progress')
-
 def train_eval(is_training):
-    global BATCH_SIZE, LEARNING_RATE, MOMENTUM, model, dataset, optimizer, eval_button, train_button, accuracy_widget, loss_widget, progress_widget, state_widget
-    
+    global BATCH_SIZE, LEARNING_RATE, MOMENTUM, model, dataset, optimizer, eval_button, train_button, accuracy_widget, loss_widget, progress_widget, state_widget  
     try:
         train_loader = torch.utils.data.DataLoader(
             dataset,
             batch_size=BATCH_SIZE,
             shuffle=True
         )
-
         state_widget.value = 'stop'
         train_button.disabled = True
         eval_button.disabled = True
         time.sleep(1)
-
         if is_training:
             model = model.train()
         else:
@@ -232,28 +176,15 @@ def train_eval(is_training):
             sum_loss = 0.0
             error_count = 0.0
             for images, labels in iter(train_loader):
-                # send data to device
                 images = images.to(device)
                 labels = labels.to(device)
-
                 if is_training:
-                    # zero gradients of parameters
                     optimizer.zero_grad()
-
-                # execute model to get outputs
                 outputs = model(images)
-
-                # compute loss
                 loss = F.cross_entropy(outputs, labels)
-
                 if is_training:
-                    # run backpropogation to accumulate gradients
                     loss.backward()
-
-                    # step optimizer to adjust parameters
                     optimizer.step()
-
-                # increment progress
                 error_count += len(torch.nonzero(outputs.argmax(1) - labels).flatten())
                 count = len(labels.flatten())
                 i += count
@@ -261,7 +192,6 @@ def train_eval(is_training):
                 progress_widget.value = i / len(dataset)
                 loss_widget.value = sum_loss / i
                 accuracy_widget.value = 1.0 - error_count / i
-                
             if is_training:
                 epochs_widget.value = epochs_widget.value - 1
             else:
@@ -269,16 +199,11 @@ def train_eval(is_training):
     except e:
         pass
     model = model.eval()
-
     train_button.disabled = False
     eval_button.disabled = False
     state_widget.value = 'live'
-    
 train_button.on_click(lambda c: train_eval(is_training=True))
 eval_button.on_click(lambda c: train_eval(is_training=False))
-    
-# We could try and run the python script here    
-    
 train_eval_widget = ipywidgets.VBox([
     epochs_widget,
     progress_widget,
@@ -286,38 +211,26 @@ train_eval_widget = ipywidgets.VBox([
     accuracy_widget,
     ipywidgets.HBox([train_button, eval_button])
 ])
-
-# display(train_eval_widget)
 print("trainer configured and train_eval_widget created")
 
 
 
 
-
-
-
-
-
-# Combine all the widgets into one display
+# Combine the widgets that we created in the steps above 
 all_widget = ipywidgets.VBox([
     ipywidgets.HBox([data_collection_widget, live_execution_widget]), 
     train_eval_widget,
     model_widget
 ])
-
 display(all_widget)
 
 
 
 
-
-
-
+# Remember to shut the camera down by running the below
 import os
 import IPython
-
 if type(camera) is CSICamera:
     print("Ignore 'Exception in thread' tracebacks\n")
     camera.cap.release()
-
 os._exit(00)
