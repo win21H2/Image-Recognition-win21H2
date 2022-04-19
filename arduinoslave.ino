1e474b249f2d08c31875fb50cc6b232c4d5c6a0b
324Hz <92825997+win21H2@users.noreply.github.com>
2022-04-19 17:43:12 -0500

    arduinoslave
    added comments for the code so that people can better understand the code and what happens

--- arduinoslave.ino
+++ arduinoslave.ino
@@ -20,44 +20,47 @@
 //OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 //USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
 int gled = 4;
 int bled = 5;
 int rled = 6;
+// initialize the three LED's with different pins
 
 void setup() {
   Serial.begin(11520);
+  // begin baud rate at 115200 because 9600 did not work with serial communication with the Jetson Nano
   pinMode(gled, OUTPUT);
   pinMode(bled, OUTPUT);
   pinMode(rled, OUTPUT);
-  while (!Serial) {
+  // sets the pin mode for every LED to OUTPUT
+  while (!Serial) { // waits for the serial port to connect
     ;
   }
 }
 void loop() {
   char buffer[16];
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0) { // checks if serial is available and if the data is more than 0
     int size = Serial.readBytesUntil('\n', buffer, 12);
     
-    if (buffer[0] == 'G') {
+    if (buffer[0] == 'G') { // this is statment checks if the first letter of the data is "G" and if so, run the code below which turns the green LED on
       digitalWrite(gled, HIGH);
     }
-    else {
+    else { // if there is no output, keep the LED off (this might not be needed)
       digitalWrite(gled, LOW);
     }
     
-    if (buffer[0] == 'B') {
+    if (buffer[0] == 'B') { // this is statment checks if the first letter of the data is "B" and if so, run the code below which turns the blue LED on
       digitalWrite(bled, HIGH);
     }
-    else {
+    else { // if there is no output, keep the LED off (this might not be needed)
       digitalWrite(bled, LOW);
     }
 
-    if (buffer[0] == 'R') {
+    if (buffer[0] == 'R') { // this is statment checks if the first letter of the data is "R" and if so, run the code below which turns the red LED on
       digitalWrite(rled, HIGH);
     }
-    else {
+    else { // if there is no output, keep the LED off (this might not be needed)
       digitalWrite(rled, LOW);
     }
+    
   }
 }
