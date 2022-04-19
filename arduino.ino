1a56c2caf68180802c840f8133f6f3e2dc0c8648
324Hz <92825997+win21H2@users.noreply.github.com>
2022-04-19 00:33:44 -0500

    arduino
    changed the code so that there are three seperate colors for the different triangles which will be recognized

--- arduino.ino
+++ arduino.ino
@@ -1,23 +1,40 @@
-// Note that this code is not the final code and there will be changes made
-// This code also might not work because the serial communications for some reason does not work and instead just flashes the LED a couple of times
+int gled = 4;
+int bled = 5;
+int rled = 6;
 
 void setup() {
-  Serial.begin(9600); // begin serial communications
-  pinMode(LED_BUILTIN, OUTPUT); // set the builtin LED as an output LED
+  Serial.begin(11520);
+  pinMode(gled, OUTPUT);
+  pinMode(bled, OUTPUT);
+  pinMode(rled, OUTPUT);
   while (!Serial) {
     ;
   }
 }
-
 void loop() {
   char buffer[16];
   if (Serial.available() > 0) {
     int size = Serial.readBytesUntil('\n', buffer, 12);
-    if (buffer[0] == 'Y') { // if anything from serial is "Y", run the code below which turns the builtin LED on
-      digitalWrite(LED_BUILTIN, HIGH); 
+    
+    if (buffer[0] == 'G') {
+      digitalWrite(gled, HIGH);
     }
-    if (buffer[0] == 'N') { // if anything from serial is "N", run the code below which turns the builtin LED off
-      digitalWrite(LED_BUILTIN, LOW);
+    else {
+      digitalWrite(gled, LOW);
+    }
+    
+    if (buffer[0] == 'B') {
+      digitalWrite(bled, HIGH);
+    }
+    else {
+      digitalWrite(bled, LOW);
+    }
+
+    if (buffer[0] == 'R') {
+      digitalWrite(rled, HIGH);
+    }
+    else {
+      digitalWrite(rled, LOW);
     }
   }
 }
